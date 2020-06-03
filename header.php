c819de98f337b5312e97033b84080a6a497c5901
Sathish <sathish.333ddd@gmail.com>
2020-06-03 18:58:40 +0530

    latest psot

---
+++ header.php
@@ -0,0 +1,141 @@
+<header class="main-menu">
+  <nav class="navbar navbar-expand-lg navbar-light sticky-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+        <img src="images/logo.png" class="img-fluid"/></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile_nav" aria-controls="mobile_nav" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span> 
+        </button>
+        <div class="collapse navbar-collapse" id="mobile_nav">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0 float-md-right">
+        </ul>
+        <ul class="navbar-nav navbar-light">
+            <li class="nav-item"><a class="nav-link" href="#">HOME</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">ABOUT</a></li>
+          
+            <!--========-->
+            <li class="nav-item dropdown megamenu-li dmenu">
+                <a class="nav-link dropdown-toggle" href="" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">DESIGN</a>
+                <div class="dropdown-menu megamenu sm-menu border-top" aria-labelledby="dropdown01">
+                    <div class="row">
+                        <div class="col-sm-6 col-lg-3 border-right mb-4">
+                           
+                            <a class="dropdown-item" href="#"><i class="fa fa-picture-o"></i> UI/UX</a>
+                            <a class="dropdown-item" href="#"><i class="fa fa-object-group"></i> WIREFRAMING</a>
+                            <a class="dropdown-item" href="#"><i class="fa fa-file-video-o"></i> PRESNTATIONAL VIDEOS</a>
+                            
+                        </div>
+                       <div class="col-sm-6 col-lg-3 border-right mb-4">
+                            
+                            <a class="dropdown-item" href="#"><i class="fa fa-wordpress"></i>WORDPRESS DESIGN</a>
+                            <a class="dropdown-item" href="#"><i class="fa fa-android"></i>ANDROID XML DESIGN</a>
+                             
+                        </div>
+                        <div class="col-sm-6 col-lg-3 border-right mb-4">
+                             
+                            <a class="dropdown-item" href="#"><i class="fa fa-apple"></i> IOS APP DESIGN</a>
+                            <a class="dropdown-item" href="#"><i class="fa fa-file-image-o"></i> GRAPHICS DESIGN</a>
+                             
+                        </div>
+                        <div class="col-sm-6 col-lg-3 mb-4">
+                            
+                            <a class="dropdown-item" href="#"><i class="fa fa-crosshairs"></i> LOGO DESIGN</a>
+                            <a class="dropdown-item" href="#"><i class="fa fa-window-maximize"></i> BROCHURE/PAMPLET DESIGN</a>
+                             
+                        </div>
+                    </div>                     
+                </div>
+            </li>
+            <!--=========-->
+
+             <!--========-->
+             <li class="nav-item dropdown megamenu-li dmenu">
+              <a class="nav-link dropdown-toggle" href="" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">DEVELOPMENT</a>
+              <div class="dropdown-menu megamenu sm-menu border-top" aria-labelledby="dropdown01">
+                  <div class="row">
+                      <div class="col-sm-6 col-lg-4 border-right mb-4">                           
+                          <a class="dropdown-item" href="#"><i class="fa fa-globe"></i> WEB APPLICATIONS</a>
+                          <a class="dropdown-item" href="#"><i class="fa fa-wordpress"></i> WORDPRESS DEVELOPMENT</a>
+                        
+                         
+                      </div>
+                     <div class="col-sm-6 col-lg-4 border-right mb-4">                         
+                     <a class="dropdown-item" href="#"><i class="fa fa-shopping-cart"></i> ECOMMERCE DEVELOPMENT</a>
+                          <a class="dropdown-item" href="#"><i class="fa fa-pencil"></i> SCHOOL MANAGEMENT APPLICATION</a>
+                      </div>
+                      <div class="col-sm-6 col-lg-4 border-right mb-4">                          
+                          <a class="dropdown-item" href="#"><i class="fa fa-wifi"></i> CUSTOM WEB APPLICATION</a>
+                          <a class="dropdown-item" href="#"><i class="fa fa-code"></i> ANGULAR APPLICATION</a>
+                      </div>
+                 
+                  </div>                     
+              </div>
+          </li>
+          <!--=========-->
+
+           <!--========-->
+           <li class="nav-item dropdown megamenu-li dmenu">
+            <a class="nav-link dropdown-toggle" href="" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">DIGITAL MARKETING</a>
+            <div class="dropdown-menu megamenu sm-menu border-top" aria-labelledby="dropdown01">
+                <div class="row">
+                    <div class="col-sm-6 col-lg-3 border-right mb-4">                         
+                        <a class="dropdown-item" href="#"><i class="fa fa-google"></i>SEARCH ENGINE OPTIMIZATION</a>
+                        <a class="dropdown-item" href="#"><i class="fa fa-share-square-o"></i> SOCIAL MEDIA MARKETING</a>
+                        <a class="dropdown-item" href="#"><i class="fa fa-pied-piper-pp"></i> GOOGLE ADS</a>
+                        
+                    </div>
+                   <div class="col-sm-6 col-lg-3 border-right mb-4">                       
+                        <a class="dropdown-item" href="#"><i class="fa fa-facebook"></i> FACEBOOK/INSTA ADS</a>
+                        <a class="dropdown-item" href="#"><i class="fa fa-youtube-play"></i> YOUTUBE ADS</a>
+                        <a class="dropdown-item" href="#"><i class="fa fa-envelope-o"></i> EMAIL MARKETING</a>
+                      
+                    </div>
+                    <div class="col-sm-6 col-lg-3 border-right mb-4">                        
+                        <a class="dropdown-item" href="#"><i class="fa fa-pencil"></i> CONTENT WRITING</a>
+                        <a class="dropdown-item" href="#"><i class="fa fa-sign-out"></i> LEAD GENERATION</a>
+                        
+                    </div>
+                    <div class="col-sm-6 col-lg-3 mb-4">                       
+                        <a class="dropdown-item" href="#"><i class="fa fa-cart-plus"></i> E-COMMERCE MARKETING</a>
+                        <a class="dropdown-item" href="#"><i class="fa fa-mobile"></i> MOBILE APP MARKETING</a>
+                      
+                    </div>
+                </div>                     
+            </div>
+        </li>
+        <!--=========-->
+
+         <!--========-->
+         <li class="nav-item dropdown megamenu-li dmenu">
+          <a class="nav-link dropdown-toggle" href="" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">MOBILE APPLICATIONS</a>
+          <div class="dropdown-menu megamenu sm-menu border-top" aria-labelledby="dropdown01">
+              <div class="row">
+                  <div class="col-sm-6 col-lg-3 border-right mb-4">                       
+                      <a class="dropdown-item" href="#"><i class="fa fa-android"></i>ANDROID NATIVE APPLICATIONS</a>
+                       
+                  </div>
+                 <div class="col-sm-6 col-lg-3 border-right mb-4">                      
+                      <a class="dropdown-item" href="#"><i class="fa fa-apple"></i>IOS APPLICATIONS</a>
+                     
+                  </div>
+                  <div class="col-sm-6 col-lg-3 border-right mb-4">                      
+                      <a class="dropdown-item" href="#"><i class="fa fa-fast-backward"></i> HYBRID APPLICATIONS</a>
+                       
+                  </div>
+                  <div class="col-sm-6 col-lg-3 mb-4">         
+                      <a class="dropdown-item" href="#"><i class="fa fa-gamepad"></i> GAME APPLICATIONS</a>
+                      
+                  </div>
+              </div>                     
+          </div>
+      </li>
+      <!--=========-->
+            <li class="nav-item"><a class="nav-link" href="#">CONTACT</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">LOGIN</a></li>
+            
+   
+        </ul>
+        </div>
+    </div>
+</nav>
+ </header>
--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,26 @@
 $(window).scroll(function(){
-    if ($(window).scrollTop() >= 300) {
-        $('.main-menu-list').addClass('fixed-header');        
+    if ($(window).scrollTop() >= 50) {
+        $('.main-menu').addClass('fixed-header');        
     }
     else {
-        $('.main-menu-list').removeClass('fixed-header');      
+        $('.main-menu').removeClass('fixed-header');      
     }
 });
+
+//Mega menu custom
+ 
+$(document).ready(function () {
+    $('.navbar-light .dmenu').hover(function () {
+            $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+        }, function () {
+            $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+        });
+    }); 
+     
+        $(document).ready(function() {
+        $(".megamenu").on("click", function(e) {
+            e.stopPropagation();
+        });
+    });
+    
+
